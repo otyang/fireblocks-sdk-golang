6da0760d4e783f90dd5c14c90e8826b0c3b09ad7
O Young <60820143+otyang@users.noreply.github.com>
2024-03-15 19:31:18 +0100

    Update dto.go

--- vault/dto.go
+++ vault/dto.go
@@ -64,3 +64,75 @@ type CreateAddressResponse struct {
 	Tag               string `json:"tag"`
 	Bip44AddressIndex int    `json:"bip44AddressIndex"`
 }
+
+
+
+type VaultAssetBalanceResponse struct {
+	ID           string          `json:"id"`
+	Total        decimal.Decimal `json:"total"`
+	Balance      decimal.Decimal `json:"balance"`
+	LockedAmount decimal.Decimal `json:"lockedAmount"`
+	Available    decimal.Decimal `json:"available"`
+	Pending      decimal.Decimal `json:"pending"`
+	Frozen       decimal.Decimal `json:"frozen"`
+	Staked       decimal.Decimal `json:"staked"`
+	BlockHeight  string          `json:"blockHeight"`
+}
+
+type ListVaultsParams struct {
+	NamePrefix         string  `json:"namePrefix"`
+	NameSuffix         string  `json:"nameSuffix"`
+	MinAmountThreshold float64 `json:"minAmountThreshold"`
+	AssetId            string  `json:"assetId"`
+	OrderBy            OrderBy `json:"orderBy"`
+	Limit              int     `json:"limit"` // min of 1 &  max of 500
+	Before             string  `json:"before"`
+	After              string  `json:"after"`
+}
+
+type OrderBy string
+
+const (
+	OrderByAsc  OrderBy = "ASC"
+	OrderByDesc OrderBy = "DESC"
+)
+
+type ListVaultsResponseAccountAssets struct {
+	ID           string `json:"id"`
+	Total        string `json:"total"`
+	Available    string `json:"available"`
+	Pending      string `json:"pending"`
+	Frozen       string `json:"frozen"`
+	LockedAmount string `json:"lockedAmount"`
+	Staked       string `json:"staked"`
+	// TotalStakedCPU       int    `json:"totalStakedCPU"`
+	// TotalStakedNetwork   string `json:"totalStakedNetwork"`
+	// SelfStakedCPU        string `json:"selfStakedCPU"`
+	// SelfStakedNetwork    string `json:"selfStakedNetwork"`
+	PendingRefundCPU     string `json:"pendingRefundCPU"`
+	PendingRefundNetwork string `json:"pendingRefundNetwork"`
+	BlockHeight          string `json:"blockHeight"`
+	BlockHash            string `json:"blockHash"`
+	RewardsInfo          struct {
+		PendingRewards string `json:"pendingRewards"`
+	} `json:"rewardsInfo"`
+}
+
+type ListVaultsResponseAccount struct {
+	ID            string                            `json:"id"`
+	Name          string                            `json:"name"`
+	Assets        []ListVaultsResponseAccountAssets `json:"assets"`
+	HiddenOnUI    bool                              `json:"hiddenOnUI"`
+	CustomerRefID string                            `json:"customerRefId"`
+	AutoFuel      bool                              `json:"autoFuel"`
+}
+
+type ListVaultsResponse struct {
+	Accounts []ListVaultsResponseAccount `json:"accounts"`
+	Paging   struct {
+		Before string `json:"before"`
+		After  string `json:"after"`
+	} `json:"paging"`
+	PreviousURL string `json:"previousUrl"`
+	NextURL     string `json:"nextUrl"`
+}
